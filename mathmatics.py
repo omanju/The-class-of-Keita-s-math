a1ae6f5a3bcabd7cc29a44c8f11d7c6d9159f070
松本恵太 <keita@ms.kouta2133.work>
2025-09-01 19:27:55 +0900

    commit detail

--- mathmatics.py
+++ mathmatics.py
@@ -1,10 +1,10 @@
-print("a^3 + b^3 + c^3 = 2026　を満たす自然数の組 (a, b, c) を全て求めよ")
+print("a^2 + b^2 + c^2 = 2026　を満たす自然数の組 (a, b, c) を全て求めよ")
 
 N = 44
 
 for a in range(1, N + 1):
     for b in range(1, N + 1):
         for c in range(1, N + 1):
-            i = a ** 3 + b ** 3 + c ** 3
+            i = a ** 2 + b ** 2 + c ** 2
             if i == 2026:
                 print(a, b, c)
